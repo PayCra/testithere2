e0d6b9982920a01556e1e68dcec4419669364040
Paula Krupa <paula.krupa@gmail.com>
2020-09-10 19:12:31 +0200

    animals

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import "./App.css";
+// import "./App.css";
 
-let Array = [];
-let Array2= [];
+let firstAnimal = prompt('Animal, please:').toLowerCase()
+let secondAnimal = prompt('Another animal, please:').toLowerCase()
+let thirdAnimal = prompt('Last animal, please:').toLowerCase()
 
-for (let i=0;i<=10;i++) {
-    Array.push(<div>{"/".repeat(i) + "\\".repeat(i)}</div>)
+let AnArray = ['a', 'e', 'i', 'o', 'u', 'y']
+
+const checkForVowel = (character, vowel) => character === vowel;
+
+let isAVowel;
+
+const ArticleAn = (character) => {
+    isAVowel = false;
+    for (let i = 0; i < AnArray.length; i++) {
+        if (checkForVowel(character, AnArray[i])) {
+            isAVowel = true;
+        }
+    }
 }
 
-for (let j=10;j===0;j--) {
-    Array2.push(<div>{"\\".repeat(j) + "/".repeat(j)}</div>)
+const Article = () => {
+    if (isAVowel===true) {
+        return 'an'
+    } else {
+        return 'a'
+    }
 }
 
-function App () {
+function App() {
     return (
-        <div className="center">{Array}{Array2}</div>
+        <>
+            <div>When you're at your best, you are like {ArticleAn(firstAnimal[0])}{Article()} {firstAnimal}.</div>
+            <div>People perceive you as similar to {ArticleAn(secondAnimal[0])}{Article()} {secondAnimal}.</div>
+            <div>{ArticleAn(thirdAnimal[0])}{'A' + Article().slice(1)} {thirdAnimal} is who you really are.</div>
+        </>
     )
-};
+}
 
 export default App;
